225d056c926f4ac2853be99357086c01f55d2042
Souradeep Guha <115440193+kuroakumanoir@users.noreply.github.com>
2022-11-28 12:51:37 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,157 @@
+""""
+Copyright © Krypton 2022 - https://github.com/kuroakumanoir
+Description:
+This is a template to create your own discord bot in python.
+
+Version: 1.0.0
+"""
+
+#The following code follows PEP8 (mostly)
+
+import asyncio
+import json
+import os
+import platform
+import random
+import sys
+from contextlib import closing
+
+import discord
+from discord import Interaction
+from discord.ext import commands, tasks
+from discord.ext.commands import Bot, Context
+
+import exceptions
+
+""" Calling your configuration file"""
+
+if not os.path.isfile("config.json"):
+    sys.exit("'config.json' not found! Please add it and try again.")
+else:
+    with open("config.json") as file:
+        config = json.load(file)
+				
+"""
+Now we will load the intents,
+You Should First read the discord.py document to understand about intents first
+"""
+intents = discord.Intents.default()
+
+bot = Bot(command_prefix=commands.when_mentioned_or(
+    config["prefix"]), intents=intents, help_command=None)
+
+bot.config = config
+
+@bot.event
+async def on_ready() -> None:
+    """
+    The code in this even is executed when the bot is ready
+    """
+    print(f"Logged in as {bot.user.name}")
+    print(f"discord.py API version: {discord.__version__}")
+    print(f"Python version: {platform.python_version()}")
+    print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    print("-------------------")
+    status_task.start()
+
+
+@tasks.loop(minutes=1.0)
+async def status_task() -> None:
+    """
+    Setup the game status task of the bot
+    """
+    statuses = ["Watching discord!", "Playing with humans!", "This place is sus!"]
+    await bot.change_presence(activity=discord.Game(random.choice(statuses)))
+
+
+@bot.event
+async def on_message(message: discord.Message) -> None:
+    """
+    The code in this event is executed every time someone sends a message, with or without the prefix
+
+    :param message: The message that was sent.
+    """
+    if message.author == bot.user or message.author.bot:
+        return
+    await bot.process_commands(message)
+
+
+@bot.event
+async def on_command_completion(context: Context) -> None:
+    """
+    The code in this event is executed every time a normal command has been *successfully* executed
+    :param context: The context of the command that has been executed.
+    """
+    full_command_name = context.command.qualified_name
+    split = full_command_name.split(" ")
+    executed_command = str(split[0])
+    if context.guild is not None:
+        print(
+            f"Executed {executed_command} command in {context.guild.name} (ID: {context.guild.id}) by {context.author} (ID: {context.author.id})")
+    else:
+        print(
+            f"Executed {executed_command} command by {context.author} (ID: {context.author.id}) in DMs")
+
+
+@bot.event
+async def on_command_error(context: Context, error) -> None:
+    """
+    The code in this event is executed every time a normal valid command catches an error
+    :param context: The context of the normal command that failed executing.
+    :param error: The error that has been faced.
+    """
+    if isinstance(error, commands.CommandOnCooldown):
+        minutes, seconds = divmod(error.retry_after, 60)
+        hours, minutes = divmod(minutes, 60)
+        hours = hours % 24
+        embed = discord.Embed(
+            title="Hey, please slow down!",
+            description=f"You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, exceptions.UserBlacklisted):
+        """
+        The code here will only execute if the error is an instance of 'UserBlacklisted', which can occur when using
+        the @checks.not_blacklisted() check in your command, or you can raise the error by yourself.
+        """
+        embed = discord.Embed(
+            title="Error!",
+            description="You are blacklisted from using the bot.",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, exceptions.UserNotOwner):
+        """
+        Same as above, just for the @checks.is_owner() check.
+        """
+        embed = discord.Embed(
+            title="Error!",
+            description="You are not the owner of the bot!",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.MissingPermissions):
+        embed = discord.Embed(
+            title="Error!",
+            description="You are missing the permission(s) `" + ", ".join(
+                error.missing_permissions) + "` to execute this command!",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        embed = discord.Embed(
+            title="Error!",
+            # We need to capitalize because the command arguments have no capital letter in the code.
+            description=str(error).capitalize(),
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    raise error
+
+
+bot.run(config["token"])
+
+
+
+
